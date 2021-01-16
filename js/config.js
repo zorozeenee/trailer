1bf4ac3509d665e1fc5427bd06715aedaa629a0a
zorozeenee <46616297+zorozeenee@users.noreply.github.com>
2021-01-16 10:27:26 -0600

    fgs

--- js/config.js
+++ js/config.js
@@ -55,7 +55,7 @@ var l_bgVideo = true;
 // Just make sure they are using webm VP8 format 
 // (Place the videos in the backgrounds/videos folder)
 // (You can also use external URL)
-var l_background = "trailer.webm";
+var l_background = "backround.webm";
 
 // (Image-background only)
 // Enable map-based background image?
